c12685b07964c4efebf921f546fb30466470930f
tommaso.corsini <tommaso.corsini@buonarroti.tn.it>
2023-10-16 08:26:26 +0200

    feat: base method + attributes

--- src/Persona.java
+++ src/Persona.java
@@ -1,2 +1,19 @@
 public class Persona {
+    private String nome, sesso;
+    private int annoNascita;
+
+    public Persona(String nome, String sesso, int annoNascita){
+        this.nome = nome;
+        this.annoNascita = annoNascita;
+        this.sesso = sesso;
+    }
+
+    public String toString(){
+        String rit = "{\n";
+        rit += "nome: "+this.nome+"\n";
+        rit += "sesso: "+this.sesso+"\n";
+        rit += "anno di nascita: "+this.annoNascita+"\n";
+        rit += "}";
+        return rit;
+    }
 }
