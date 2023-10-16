592d0f00772a04543ad2f35d4e70cbcbc936c391
tommaso.corsini <tommaso.corsini@buonarroti.tn.it>
2023-10-16 08:44:59 +0200

    test: clone 'Persona' and print p2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Persona p = new Persona("T", "Maschio", 2006);
         System.out.println(p.toString());
+        Persona p2 = (Persona) p.clone();
+        p2.setSesso("Fimmina");
+        System.out.println(p2.toString());
     }
 }
