2370565865501237382da9547fec4d10737e0ffc
tommaso.corsini <tommaso.corsini@buonarroti.tn.it>
2023-10-16 09:49:45 +0200

    fix: test and Costructor class 'Persona'

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Persona p = new Persona("T", "Maschio", 2006);
+        Persona p = new Persona("T", "Maschio", 2006, 46);
         System.out.println(p.toString());
         Persona p2 = (Persona) p.clone();
         p2.setSesso("Fimmina");
