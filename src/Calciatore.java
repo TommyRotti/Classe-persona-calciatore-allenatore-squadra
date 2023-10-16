ddfcef746d9bd5c5023ab05221b6997321a72263
tommaso.corsini <tommaso.corsini@buonarroti.tn.it>
2023-10-16 08:49:15 +0200

    fix: extends Calciatore

--- src/Calciatore.java
+++ src/Calciatore.java
@@ -1,3 +1,8 @@
-public class Calciatore {
+public class Calciatore extends Persona{
 
+    private String ruolo;
+    private int nMaglia, nGol;
+    public Calciatore(String nome, String sesso, int annoNascita, String ruolo, int nMaglia, int nGol) {
+        super(nome, sesso, annoNascita);
+    }
 }
