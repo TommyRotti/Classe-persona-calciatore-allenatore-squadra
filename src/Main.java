ed94ed18ddea89321b18034c93499c8ee8ac9c3a
tommaso.corsini <tommaso.corsini@buonarroti.tn.it>
2023-10-16 08:29:47 +0200

    test: print person

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona p = new Persona("T", "Maschio", 2007);
+        System.out.println(p.toString());
     }
 }
