d6b2ce8d8ee1d3a0e9d98ae29c5d97615bee7a47
tommaso.corsini <tommaso.corsini@buonarroti.tn.it>
2023-10-16 08:51:32 +0200

    feat: attributes and Costructor class 'Calciatore'

--- src/Calciatore.java
+++ src/Calciatore.java
@@ -2,7 +2,8 @@ public class Calciatore extends Persona{
 
     private String ruolo;
     private int nMaglia, nGol;
-    public Calciatore(String nome, String sesso, int annoNascita, String ruolo, int nMaglia, int nGol) {
+    public Calciatore(String nome, String sesso, int annoNascita,
+                      String ruolo, int nMaglia, int nGol) {
         super(nome, sesso, annoNascita);
     }
 }
